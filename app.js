fe0aacf6014a67a0d5c8363873235896962cc63f
Piotr Dudek <piotr72@gmail.com>
2020-10-11 16:21:51 -0400

    consolidate all questions in one array

--- app.js
+++ app.js
@@ -16,117 +16,189 @@ const idArray = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-
-var questions = [{
-    name: "name",
-    message: "What is your manager's name?",
-    type: "input",
-    validate: answer => {
-        if (answer !== "") {
-            return true;
+const questions = [
+    {
+        name: "name",
+        message: "What is your Manager's name?",
+        type: "input",
+        validate: answer => {
+            if (answer !== "") {return true}
+            return "This field can't be empty";
         }
-        return "This field can't be empty";
-    }
-}, 
-{
-    name: "id",
-    message: "What is your manager's id?",
-    type: "input",
-    validate: function (value) {
-        var pass = value.match(
-        /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
-        );
-        if (pass) {
-        return true;
-        }
-
-        return 'Please enter a valid number';
     },
-
-},
-{
-    name: "email",
-    message: "What is manager's email?",
-    type: "input"
-},
-{
-    name: "officeNumber",
-    message: "What is your manager's office number?",
-    type: "input",
-},
-{
-    name: "teamMember",
-    message: "What type of team member would you like to add?",
-    type: "list",
-    choices: ["Engineer", "Intern", "I don't want to add any more team members"]
-}
-
+    {
+        name: "id",
+        message: "What is your Manager's ID?",
+        type: "input",
+        // validate: value => {
+        //     var pass = value.match(/^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i);
+        //     if (pass) {return true}
+        //     return 'Please enter a valid number';
+        // },
+    },
+    {
+        name: "email",
+        message: "What is Manager's email?",
+        type: "input"
+    },
+    {
+        name: "officeNumber",
+        message: "What is your Manager's office number?",
+        type: "input",
+    },
+    {
+        name: "teamMember",
+        message: "What type of team member would you like to add?",
+        type: "list",
+        choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+    },
+    {
+        name: "name",
+        message: "What is your Engineer's name?",
+        type: "input",
+        validate: answer => {
+            if (answer !== "") {return true}
+            return "This field can't be empty";
+        }
+    },
+    {
+        name: "id",
+        message: "What is your Engineer's ID?",
+        type: "input",
+        validate: answer => {
+            const pass = answer.match(
+                /^[1-9]\d*$/
+            );
+            if(pass) {
+                if(idArray.includes(answer)) {
+                    return "This ID is already taken."
+                }
+                else {
+                    return true;
+                }
+            }
+            return "Please enter a positive number greater than zero."
+        }
+    },
+    {
+        name: "email",
+        message: "What is Engineer's email?",
+        type: "input"
+    },
+    {
+        name: "github",
+        message: "What is your Engineer's github profile?",
+        type: "input"
+    },
+    {
+        name: "name",
+        message: "What is your Intern's name?",
+        type: "input",
+        validate: answer => {
+            if (answer !== "") {return true}
+            return "This field can't be empty";
+        }
+    },
+    {
+        name: "id",
+        message: "What is your Intern's ID?",
+        type: "input",
+    },
+    {
+        name: "email",
+        message: "What is Intern's email?",
+        type: "input"
+    },
+    {
+        name: "school",
+        message: "What is your Intern's school?",
+        type: "input"
+    }
 ]
 
-async function askQuestions() {
-const answers = await inquirer.prompt(questions);
-console.log(answers);
+function createManager(){
+    inquirer.prompt(
+        [
+            questions[0], questions[1], questions[2], questions[3]
+    ]).then(answers => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        teamMembers.push(manager);
+        idArray.push(answers.id);
+        chooseTeam()
+    }
+        )
+        .catch(error => {
+            if(error.isTtyError) {console.log("wrong")}
+            else {console.log("somethign else wrong")}
+        })
 
-// new Employee(answers.name, answers.id, answers.email)
-// console.log(new Employee);
-
-// const htmlString = passAnswers(answers);
 }
 
-// inquirer
-// .prompt([
-//     {
-//         name: "name",
-//         message: "What is your manager's name?",
-//         type: "input",
-//         validate: answer => {
-//             if (answer !== "") {
-//                 return true;
-//             }
-//             return "This field can't be empty";
-//         }
-//     }, 
-//     {
-//         name: "id",
-//         message: "What is your manager's id?",
-//         type: "input",
-//         validation: answer => {
-//             const numbers = /^[0-9]+$/;
-//             if (answer.value.match(numbers)) {
-//                 return true;
-//             }
-//             return false;
-//         }
-    
-//     },
-//     {
-//         name: "email",
-//         message: "What is manager's email?",
-//         type: "input"
-//     },
-//     {
-//         name: "officeNumber",
-//         message: "What is your manager's office number?",
-//         type: "input"
-//     }
-// ])
-// .then(
-//     answers => {
-//         console.log(answers);
-//     }
-// )
-// .catch( error => {
-//     if(error.isTtyError) {
-//         console.log("wrong");
-//     }
-//     else {
-//         console.log("success");
-//     }
-// });
+// async function createEngineer(questions) {
+//     const answers = await inquirer.prompt(questions);
+//     const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+//     teamMembers.push(engineer);
+//     idArray.push(answers.id);
+//     chooseTeam();
+// }
 
 
+function chooseTeam() {
+    inquirer.prompt(
+        [
+            questions[4]
+    ]).then(answer => {
+    if(answer.teamMember === "Engineer") {
+        addEngineer();
+    }
+    else if (answer.teamMember === "Intern") {
+        addIntern();
+    }
+    else {buildTeam()}
+}
+    )
+        
+}
 
-askQuestions();
+function addEngineer() {
+    inquirer.prompt(
+        [
+            questions[5], questions[6], questions[7], questions[8]
+    ]).then(answers => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        teamMembers.push(engineer);
+        idArray.push(answers.id);
+        chooseTeam()}
+        )
+        .catch(error => {
+            if(error.isTtyError) {console.log("wrong")}
+            else {console.log("somethign else wrong")}
+        })
+}
+
+function addIntern() {
+    inquirer.prompt(
+        [
+            questions[9], questions[10], questions[11], questions[12]
+    ]).then(answers => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        teamMembers.push(intern);
+        idArray.push(answers.id);
+        chooseTeam()}
+        )
+        .catch(error => {
+            if(error.isTtyError) {console.log("wrong")}
+            else {console.log("somethign else wrong")}
+        })
+}
+
+createManager();
+
+function buildTeam() {
+    console.log(teamMembers);
+    console.log(idArray);
+    console.log("ready to build team")
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
